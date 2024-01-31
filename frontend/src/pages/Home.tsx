31990bd462a0dfcc7670fad1c52f3288204d62a5
kathyychenn <ktchen@ucsd.edu>
2024-01-31 13:36:46 -0800

    update faq button and other text

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,62 +7,40 @@ export function Home() {
       <h1 className={styles.title}>Become a Certified Interior Designer</h1>
       <img alt="" src="/home_cover.svg" />
       <div className={styles.textDivider}>
-        <h2 className={styles.heading}>Candidate Information</h2>
+        <h2 className={styles.heading}>CID Candidate Information</h2>
       </div>
       <ul className={styles.list}>
-        <li>
-          <a href="https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?sectionNum=5800.&lawCode=BPC">
-            California Business & Professions Codes Sections 5800 - 5812
-          </a>
-        </li>
         <li>
           <p className={styles.item}>
             Under the Certified Interior Designers Title Act law, Certified Interior Designers are
             qualified by the California Council for Interior Design Certification (CCIDC) upon
-            evidence of a combination of interior design education and/or experience and passage of
-            the <span className={styles.red}>IDEX California®</span> examination.
+            evidence of a combination of interior design education, experience <strong>AND</strong>{" "}
+            passage of the IDEX® California examination.
           </p>
         </li>
         <li>
           <p className={styles.item}>
-            <strong>Only</strong> CCIDC can determine eligibility to be a Certified Interior
-            Designer in the State of California. Designers who meet the education, experience and
-            examination criteria of the CCIDC Board are allowed to use the “
-            <span className={styles.red}>Certified Interior Designer</span>” title and “
-            <span className={styles.red}>CID</span>” appellation and are recognized in the State of
-            California.
+            <strong>ONLY</strong> CCIDC can determine eligibility to be a Certified Interior
+            Designer in California. Designers who meet the criteria of the CCIDC are allowed to use
+            the “Certified Interior Designer” title and “CID” appellation and are recognized in the
+            State of California.
           </p>
         </li>
         <li>
           <p className={styles.item}>
-            Upon qualification, the “Certified Interior Designer” will be identified by an
-            individual number which will appear on a certificate, a stamp with which to identify
-            interior design nonstructural and non-seismic drawings and documents, and an
-            identification card.
+            Upon qualification, the Certified Interior Designer will be identified by an individual
+            number on a certificate, a stamp to identify interior design nonstructural and
+            non-seismic drawings and documents, and an identification card (ID).
           </p>
         </li>
         <li>
           <p className={styles.item}>
-            Certified Interior Designers, as mandated by the State of California, have met high
-            standards of qualification and have agreed to uphold a strict code of ethics and
-            conduct.
-          </p>
-        </li>
-        <li>
-          <p className={styles.item}>
-            Certification in California is a Title Act, it is not a license or registration
-            (Practice Act). The Certified Interior Designer’s Title Act is the only thing recognized
-            by the state.
+            Certification in California is a Title Act, not a license or registration (Practice
+            Act). The Certified Interior Designer’s Title Act is the only thing the state
+            recognizes.
           </p>
         </li>
       </ul>
-      <p className={styles.message}>
-        <strong className={styles.red}>UPDATE: </strong>
-        As of January 1, 2023, Meazure Learning Live Remote Proctoring (LRP) will no longer accept
-        expired IDs. We will require all test takers without an accommodation to present a current,
-        unexpired ID for identity verification. This LRP policy now aligns with Meazure Learning’s
-        Test Center policy.
-      </p>
       <button className={styles.apply} type="button">
         Apply Now
       </button>
@@ -71,12 +49,21 @@ export function Home() {
       </div>
 
       <p>
-        To be eligible for the
-        <span className={styles.red}> IDEX® California</span> and begin the certification process,
-        a candidate must provide evidence of completing the required education and/or diversified
-        design work experience in interior design or architectural work. Candidates must meet the
-        minimum education/experience requirements to apply. Each path and their unique
-        qualifications are listed below.
+        To become a CID, you must:
+        <ul className={styles.list}>
+          <li>
+            Have a minimum of 6 years of combined accredited interior design education and
+            experience <strong>OR</strong> a minimum of 8 years of combined interior design
+            education and experience.
+          </li>
+          <li>
+            Provide evidence of competing the required education and/or diversified design work
+            experience in interior design, architecture, or building design.
+          </li>
+          <li>Pass the IDEX® California Exam.</li>
+        </ul>
+        There are four paths to qualification, candidates must meet one of the following path
+        criteria and each is listed with proper qualifications below:
       </p>
 
       <div className={styles.pathwaysContainer}>
@@ -85,45 +72,77 @@ export function Home() {
             <h2 className={styles.pathName}>Path 1</h2>
             <div className={styles.pathwayText}>
               <span className={styles.red}>Qualifications:</span>
-              {" >"} 40 Core Units or 5 years of Diversified Design Experience.
+              Candidates who meet the education requirement, and have <strong>NOT</strong> met the
+              work experience requirement, may opt first to apply to take the IDEX® California
+              Exam. Candidates must meet one of these qualifications to sit for the IDEX®
+              California Exam:
+              <ol className={styles.olist}>
+                <li>
+                  At least 40 semester core units from an accredited design program (60 quarter
+                  units)
+                </li>
+                <li>5-8 years of diversified design experience</li>
+              </ol>
             </div>
-            <button className={styles.learnMoreBtn} type="button">
-              Learn More
-            </button>
           </div>
           <div className={styles.pathwayContainer}>
             <h2 className={styles.pathName}>Path 2</h2>
             <div className={styles.pathwayText}>
               <span className={styles.red}>Qualifications: </span>
-              Meet education and/or work experience requirements at the time of application.
+              Candidates who meet both the education <strong>AND</strong> work experience
+              requirements will be eligible to complete the certification process upon completing
+              the IDEX® California Exam if they meet one of these requirements:
+              <ol className={styles.olist}>
+                <li>
+                  80+ semester core units from an accredited design program (120+ quarter units)
+                </li>
+                <li>
+                  60 - 79 semester core units from an accredited design program (90+ quarter units)
+                </li>
+                <li>
+                  40 - 59 semester core units from an accredited design program (60+ quarter units)
+                </li>
+                <li>
+                  At least 8 years of diversified design experience, interior design education, or
+                  combination that totals 8 years.
+                </li>
+              </ol>
             </div>
-            <button className={styles.learnMoreBtn} type="button">
-              Learn More
-            </button>
           </div>
         </div>
         <div className={styles.pathwayRows}>
           <div className={styles.pathwayContainer}>
             <h2 className={styles.pathName}>Path 3</h2>
             <div className={styles.pathwayText}>
-              <span className={styles.red}>Qualifications:</span> Meet education and/or work
-              experience requirements and have successfully passed one of the qualifying National
-              Interior Design Exams at the time of application.
+              <span className={styles.red}>Qualifications:</span> Candidates who meet the education,
+              work experience requirements, <strong>AND</strong> have successfully passed one of the
+              qualifying National Interior Design Exams at the time of application, will be eligible
+              to complete the certification process upon completing the IDEX® California Exam.
+              Qualifying exams include:
+              <ul className={styles.list}>
+                <li>ARE, CASp, LEED | Minimum AP, NCBDC, NCIDQ, NKBA | Minimum CKBD</li>
+              </ul>
+              <br />
+              RIDQC Exam Candidates, please apply under <strong>Path 1 or 2</strong>
             </div>
-            <button className={styles.learnMoreBtn} type="button">
-              Learn More
-            </button>
           </div>
           <div className={styles.pathwayContainer}>
             <h2 className={styles.pathName}>Path 4</h2>
             <div className={styles.pathwayText}>
-              <span className={styles.red}>Qualifications:</span> Mainly practice commercial design,
-              meet education and/or work experience requirements, and passed one of the qualifying
-              National Interior Design Exams at the time of application.
+              <span className={styles.red}>Qualifications:</span> Candidates who mainly practice
+              commercial design meet the education, work experience requirements
+              <strong>AND</strong> have successfully passed 1 of the qualifying National Interior
+              Design Exams at the time of application will be eligible to complete the certification
+              process with a commercial designation upon successful completion of the IDEX®
+              California Exam and passing required ICC Courses.
+              <br />
+              Qualifying exams include:
+              <ul className={styles.list}>
+                <li>ARE, CASp, LEED | Minimum AP, NCBDC, NCIDQ, NKBA | Minimum CKBD</li>
+              </ul>
+              <br />
+              RIDQC Exam Candidates, please apply under <strong>Path 1 or 2</strong>
             </div>
-            <button className={styles.learnMoreBtn} type="button">
-              Learn More
-            </button>
           </div>
         </div>
       </div>
@@ -131,33 +150,39 @@ export function Home() {
       <h1 className={styles.title}>FAQ’s</h1>
 
       <div className={styles.FaqsContainer}>
+        <div className={styles.line} />
         <div className={styles.faqBar}>
           <button className={styles.moreInfo} type="button">
             <img alt="" src="/moreInfoButton.svg" />
           </button>
           <p className={styles.item}>Certification Information & Overview</p>
         </div>
+        <div className={styles.line} />
+
         <div className={styles.faqBar}>
           <button className={styles.moreInfo} type="button">
             <img alt="" src="/moreInfoButton.svg" />
           </button>
           <p className={styles.item}>Certification Qualification & Paths</p>
         </div>
+        <div className={styles.line} />
+
         <div className={styles.faqBar}>
           <button className={styles.moreInfo} type="button">
             <img alt="" src="/moreInfoButton.svg" />
           </button>
           <p className={styles.item}>Becoming Certified</p>
         </div>
+        <div className={styles.line} />
+
         <div className={styles.faqBar}>
           <button className={styles.moreInfo} type="button">
             <img alt="" src="/moreInfoButton.svg" />
           </button>
           <p className={styles.item}>What You Need to Apply</p>
         </div>
+        <div className={styles.line} />
       </div>
     </Page>
   );
 }
-
-export default Home;
