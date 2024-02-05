24d8d2365a2bbb58e42fdf928ae2a1c397d9ab2e
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2024-02-05 10:59:58 -0800

    underline nav click, import fonts

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,18 +4,30 @@ import cartIcon from "../assets/cart.svg";
 import searchIcon from "../assets/search.svg";
 import Button from "./Button";
 export default function NavBar() {
+  function onNavigate(event) {
+    const target = event?.target;
+    if (!target || !(target instanceof HTMLElement)) return; // TODO: alert error to user
+
+    const parent = target?.parentElement;
+
+    if (!parent) return; // TODO: alert error to user
+
+    Array.from(parent.children).map((c) => c.classList.remove(styles.active));
+    target.classList.add(styles.active);
+  }
+
   return (
     <div className={styles.navBar}>
       <div className={styles.navContent}>
         <img src={logo} className={styles.logo} />
         <div className={styles.navigation}>
-          <div>Consumers</div>
-          <div>Candidates</div>
-          <div>CIDS</div>
-          <div>About Us</div>
-          <div>Resources</div>
-          <div>Contact Us</div>
-          <div>Login</div>
+          <div onClick={onNavigate}>Consumers</div>
+          <div onClick={onNavigate}>Candidates</div>
+          <div onClick={onNavigate}>CIDS</div>
+          <div onClick={onNavigate}>About Us</div>
+          <div onClick={onNavigate}>Resources</div>
+          <div onClick={onNavigate}>Contact Us</div>
+          <div onClick={onNavigate}>Login</div>
           <img src={cartIcon} className={styles.navIcon}></img>
           <img src={searchIcon} className={styles.navIcon}></img>
           <Button>Apply</Button>
