8c86ac745332ee60c5ef7813108b950d109ca2e1
Jared Rosas <jaredsbt@gmail.com>
2024-02-23 23:46:33 -0800

    FAQ's v2

--- frontend/src/components/Accordion.tsx
+++ frontend/src/components/Accordion.tsx
@@ -1,45 +1,48 @@
 import { FC, ReactNode, useEffect, useRef, useState } from "react";
 
-import clickedFAQ from "../assets/clickedFAQ.svg";
-import moreInfo from "../assets/moreInfoButton.svg";
+import selectFAQ from "../assets/selectFAQ.svg";
+import unselectFAQ from "../assets/unselectFAQ.svg";
 import styles from "../stylesheets/Home.module.css";
 
-export const Accordion: FC<{ title: string; content: ReactNode }> = ({ title, content }) => {
-  const [toggle, setIsOpen] = useState(false);
-  const [height, setHeight] = useState("0px");
+export const Accordion: FC<{
+  title: string;
+  content: ReactNode;
+  toggleAll: boolean;
+}> = ({ title, content, toggleAll }) => {
+  const [expandAllCertificationInfo, setExpandAllCertificationInfo] = useState(false);
+  const [toggle, setIsOpen] = useState(toggleAll);
+  const [height, setHeight] = useState(toggleAll || toggle ? "auto" : "0px");
   const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (contentRef.current) {
-      setHeight(toggle ? `${contentRef.current.scrollHeight}px` : "0px");
+      setHeight(toggle || toggleAll ? `${contentRef.current.scrollHeight}px` : "0px");
     }
-  }, [toggle]);
+  }, [toggle, toggleAll]);
 
   const toggleAccordion = () => {
-    setIsOpen(!toggle);
+    if (toggleAll) {
+      setIsOpen(!toggleAll);
+      setExpandAllCertificationInfo(!expandAllCertificationInfo);
+    } else {
+      setIsOpen(!toggle);
+    }
   };
 
   return (
     <>
       <div className={styles.faqBar} onClick={toggleAccordion}>
-        <button onClick={toggleAccordion} className={styles.moreInfo} type="button">
-          <img
-            className={toggle ? styles.active : "/clickedFAQ.svg"}
-            alt=""
-            src={toggle ? clickedFAQ : moreInfo}
-          />
+        <span style={{ fontWeight: 500 }}>{title}</span>
+        <button onClick={toggleAccordion} style={{ marginLeft: "auto" }} type="button">
+          <img alt="" src={toggle || toggleAll ? unselectFAQ : selectFAQ} />
         </button>
-        <p className={styles.item}>{title}</p>
       </div>
-      <div
-        className={toggle ? "collapse" : "close"}
-        style={{ height }}
-        ref={contentRef}
-        aria-hidden={!toggle}
-      >
-        {toggle && content}
+      <div style={{ height }} ref={contentRef}>
+        {(toggle || toggleAll) && content}
       </div>
-      <hr style={{ border: "1px solid #d8d8d8" }}></hr>
+      <hr
+        style={{ border: "1px solid #d8d8d8", boxShadow: "20px 2px 4px rgba(0, 0, 0, 0.1)" }}
+      ></hr>
     </>
   );
 };
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,5 +3,6 @@ import { Button } from "./Button.tsx";
 import { NavBar } from "./NavBar.tsx";
 import { Page } from "./Page.tsx";
 import { Pathway } from "./Pathway.tsx";
+import { FAQs } from "./FAQs.tsx";
 
-export { Page, Button, NavBar, Accordion, Pathway };
+export { Page, Button, NavBar, Accordion, Pathway, FAQs };
