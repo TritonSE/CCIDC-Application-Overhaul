cb95fa5427dab52fcdf0907198ab108b792e92d6
emmaz12 <ezc7288@gmail.com>
2024-02-26 16:16:20 -0800

    page1-path1/2
    created page 1 of path 1 and 2 application page

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 import { Button } from "./Button.tsx";
 import { NavBar } from "./NavBar.tsx";
+import { Path2Page1 } from "./Path2Page1.tsx";
 
-export { Button, NavBar };
+export { Button, NavBar, Path2Page1 };
--- frontend/src/pages/Apply.tsx
+++ frontend/src/pages/Apply.tsx
@@ -1,3 +1,11 @@
+import { Path2Page1 } from "../components/index.ts";
+
+
 export function Apply() {
-  return <h2>Apply</h2>;
+  return (
+    <>
+    <h2>Apply</h2>
+    <Path2Page1></Path2Page1>
+    </>
+  );
 }
