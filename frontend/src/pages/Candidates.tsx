3c34e60dcdc98f691eac21b57841d98f0b9e9297
Tikpai <hpai@ucsd.edu>
2024-02-27 22:49:04 -0800

    Removed irrelevant files

--- frontend/src/pages/Candidates.tsx
+++ frontend/src/pages/Candidates.tsx
@@ -1,11 +1,9 @@
 import CongratulationsPopupWindow from "../components/CongratulationsPopupWindow.tsx";
-import FileUploadPopupWindow from "../components/FileUploadPopupWindow.tsx";
 
 export function Candidates() {
   return (
     <>
       <h2>Candidates</h2>
-      <FileUploadPopupWindow buttonText="Upload Files" />
 
       <CongratulationsPopupWindow buttonText="Congratulations Window" />
     </>
