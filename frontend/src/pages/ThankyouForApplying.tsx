e351391c46ada1e0ee7e6491bde940944463f14a
Jared Rosas <jaredsbt@gmail.com>
2024-03-11 11:33:33 -0700

    Margin spacing above title

--- frontend/src/pages/ThankyouForApplying.tsx
+++ frontend/src/pages/ThankyouForApplying.tsx
@@ -4,12 +4,15 @@ import { Button } from "../components/Button";
 export function ThankyouForApplying() {
   return (
     <div className={styles.thankYouContainer}>
-      <h1 className={styles.title}>Thank you for Applying!</h1>
+      <div className={styles.title}>Thank you for Applying!</div>
 
       <div className={styles.message}>
         Your application seems to be missing a necessary component. Please review the requirements
         for each pathway{" "}
-        <a href="" className={styles.lightBlue}>
+        <a
+          href="https://ccidc.org/becoming-a-certified-interior-designer/"
+          className={styles.lightBlue}
+        >
           here
         </a>{" "}
         and reapply when you have met the requirements for at least path one.{" "}
