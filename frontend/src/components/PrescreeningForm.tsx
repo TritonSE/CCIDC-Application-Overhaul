162f917b21194d36aafe483e63be8829773572ed
mbansal03 <122562063+mbansal03@users.noreply.github.com>
2024-03-10 17:52:48 -0700

    minor bug fixes

--- frontend/src/components/PrescreeningForm.tsx
+++ frontend/src/components/PrescreeningForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import styles from "./PrescreeningForm.module.css"
+import styles from "./PrescreeningForm.module.css";
 
 import { Button, Page } from "./index.ts";
 
@@ -21,81 +21,84 @@ export function PrescreeningForm() {
 
   return (
     <Page>
-
       <h1 className={styles.title}>Prescreening Questions</h1>
-      <p className={styles.body}>Please answer the following questions about your experience as an Interior Designer. 
-        Based on your responses, we will automatically direct you 
-        into the appropriate pathway. View more information about 
-        the pathways here. 
-      </p>  
+      <p className={styles.body}>
+        Please answer the following questions about your experience as an Interior Designer. Based
+        on your responses, we will automatically direct you into the appropriate pathway. View more
+        information about the pathways here.
+      </p>
 
       <form onSubmit={handleSubmit}>
-
         <div>
-        <p className={styles.question}>1. Please select the appropriate amount of experience you currently have.</p>
-        
-        <input type="radio" id="op11" name="question1" />
-        <label htmlFor="op11">5+ years of Diversified Design Experience and/or 40+ Core Units</label>
-        
-        <br></br>
+          <p className={styles.question}>
+            1. Please select the appropriate amount of experience you currently have.
+          </p>
 
-        <input type="radio" id="op12" name="question1" />
-        <label htmlFor="op12">I do not have 5 years of Diversified Design Experience 
-        and/or 40+ Core Units but am in the process of completing one of these two 
-        and will do within a year from today.  
-        </label>
+          <input type="radio" id="op11" name="question1" />
+          <label htmlFor="op11">
+            5+ years of Diversified Design Experience and/or 40+ Core Units
+          </label>
 
-        <br></br>
+          <br></br>
+          <br></br>
 
-        <input type="radio" id="op13" name="question1" />
-        <label htmlFor="op13">I do not have 5 years of Diversified Design Experience 
-        or have 40+ Core Units and will not complete at least one of these two 
-        requirements within a year from today.
-        </label>
+          <input type="radio" id="op12" name="question1" />
+          <label htmlFor="op12">
+            I do not have 5 years of Diversified Design Experience and/or 40+ Core Units but am in
+            the process of completing one of these two and will do within a year from today.
+          </label>
+
+          <br></br>
+          <br></br>
+
+          <input type="radio" id="op13" name="question1" />
+          <label htmlFor="op13">
+            I do not have 5 years of Diversified Design Experience or have 40+ Core Units and will
+            not complete at least one of these two requirements within a year from today.
+          </label>
         </div>
 
         <div>
-        <p className={styles.question}>  
-        2. If you have passed any of the following qualifying National Interior Design Exams, please indicate so      
-          <ul className="bullet-list">
+          <p className={styles.question}>
+            2. If you have passed any of the following qualifying National Interior Design Exams,
+            please indicate so
+            <ul className="bullet-list">
               <li>- ARE</li>
               <li>- CASP</li>
               <li>- LEED - AP (+)</li>
               <li>- NCBDC</li>
               <li>- NCIDQ</li>
               <li>- NKBA - CKBD (+)</li>
-          </ul>
-        </p>
-        
-        <input type="radio" id="op21" name="question2" />
-        <label htmlFor="op11">Yes</label>
-        
-        <br></br>
+            </ul>
+          </p>
 
-        <input type="radio" id="op22" name="question2" />
-        <label htmlFor="op12">No</label>
+          <input type="radio" id="op21" name="question2" />
+          <label htmlFor="op21">Yes</label>
 
+          <br></br>
+          <br></br>
+
+          <input type="radio" id="op22" name="question2" />
+          <label htmlFor="op22">No</label>
         </div>
 
         <div>
-        <p className={styles.question}>3. Do you mainly practice commercial design?</p>
-        
-        <input type="radio" id="op31" name="question3" />
-        <label htmlFor="op11">Yes</label>
-        
-        <br></br>
+          <p className={styles.question}>3. Do you mainly practice commercial design?</p>
 
-        <input type="radio" id="op32" name="question3" />
-        <label htmlFor="op12">No</label>
+          <input type="radio" id="op31" name="question3" />
+          <label htmlFor="op31">Yes</label>
 
+          <br></br>
+          <br></br>
+
+          <input type="radio" id="op32" name="question3" />
+          <label htmlFor="op32">No</label>
         </div>
 
         <NavLink className={styles.centeredContainer} to={"/prescreening"}>
-                <Button onClick={null}>Continue</Button>
+          <Button onClick={null}>Continue</Button>
         </NavLink>
-
       </form>
-
     </Page>
   );
 }
