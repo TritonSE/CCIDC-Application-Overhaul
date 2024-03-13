b11db881aaf0ccbfc48a134b1077957bc7eea02e
Sidhant Rohatgi <rohatgisidhant@gmail.com>
2024-03-12 21:06:08 -0700

    Fixed Path Logic, Added Bolded Text
    Changed Submit button to use the default button with the Button component's CSS.

--- frontend/src/components/PrescreeningForm.tsx
+++ frontend/src/components/PrescreeningForm.tsx
@@ -1,35 +1,66 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./PrescreeningForm.module.css";
+import buttonStyle from "./Button.module.css";
 
-import { Button, Page } from "./index.ts";
+import { Page } from "./index.ts";
 
-// If Bullet and Checkbox have specific prop types,
-// you should import and use those types here for setExperience and setExamsPassed functions.
 
 export function PrescreeningForm() {
-  const [experience, setExperience] = useState<string>("");
-  const [examsPassed, setExamsPassed] = useState<string[]>([]); // Assuming examsPassed is an array of strings
-  const [commercialDesign, setCommercialDesign] = useState<string>("");
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Assuming you'll add the logic to post data to the server here.
-    console.log({ experience, examsPassed, commercialDesign });
+  // State to manage the selected values for each question
+  const [question1Value, setQuestion1Value] = useState("");
+  const [question2Value, setQuestion2Value] = useState("");
+  const [question3Value, setQuestion3Value] = useState("");
+
+  const pathOne = "/path1";
+  const pathTwo = "/path2";
+  const pathThree = "/path3";
+  const pathFour= "/path4";
+  const pathNQ = "/pathNQ";
+
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    let finalPath = "";
+
+    // Use the selected values to determine the finalPath
+    // Can modify this logic based on your requirements
+    if (question1Value === "op12" && question2Value === "op22" && question3Value === "op32") {
+      finalPath = pathOne;
+    }
+    else if (question1Value === "op11" && question2Value === "op22" && question3Value === "op32") {
+      finalPath = pathTwo;
+    }   
+    else if (question1Value === "op11" && question2Value === "op21" && question3Value === "op32") {
+      finalPath = pathThree;
+    }
+    else if (question1Value === "op11" && question2Value === "op21" && question3Value === "op31") {
+      finalPath = pathFour;
+    }
+    else {
+      finalPath = pathNQ;
+    }
+
+    // setFinalPath(finalPath);
+
+    navigate(finalPath);
   };
 
   return (
     <Page>
       <h1 className={styles.title}>Prescreening Questions</h1>
-      <p className={styles.body}>
+      <p className={styles.headingtwo}>
         Please answer the following questions about your experience as an Interior Designer. Based
         on your responses, we will automatically direct you into the appropriate pathway. View more
         information about the pathways here.
       </p>
       
-
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleFormSubmit} id="form1">
+        {/* Question 1 */}
         <div>
           <p className={styles.question}>
             1. Please select the appropriate amount of experience you currently have.
@@ -37,75 +68,103 @@ export function PrescreeningForm() {
 
           <br></br>
 
-          <input type="radio" id="op11" name="question1" />
+          <input type="radio" id="op11" name="question1" 
+          checked={question1Value === "op11"} 
+          onChange={() => setQuestion1Value("op11")} />
+
           <label htmlFor="op11" className={styles.label}>
-            5+ years of Diversified Design Experience and/or 40+ Core Units
+          <p>
+          5+ years of Diversified Design Experience <strong> and/or </strong> 40+ Core Units 
+          </p>
           </label>
-
           
           <br></br>
 
-          <input type="radio" id="op12" name="question1" />
+          <input type="radio" id="op12" name="question1" 
+          checked={question1Value === "op12"}
+          onChange={() => setQuestion1Value("op12")} />
+
           <label htmlFor="op12" className={styles.label}>
-            I do not have 5 years of Diversified Design Experience and/or 40+ Core Units but am in
-            the process of completing one of these two and will do within a year from today.
+          <p>
+            I do not have 5 years of Diversified Design Experience <strong> and/or </strong> 40+ Core Units but am in
+            the process of completing <strong> one </strong> of these two and will do <strong> within a year </strong> from today.
+          </p>
           </label>
 
           
           <br></br>
 
-          <input type="radio" id="op13" name="question1" />
+          <input type="radio" id="op13" name="question1" 
+          checked={question1Value === "op13"}
+          onChange={() => setQuestion1Value("op13")} />
 
           <label htmlFor="op13" className={styles.label}>
-            I do not have 5 years of Diversified Design Experience or have 40+ Core Units and will
-            not complete at least one of these two requirements within a year from today.
+          <p>
+            I do not have 5 years of Diversified Design Experience or have 40+ Core Units and 
+            <strong> will not complete </strong> at least one of these two requirements <strong> within a year </strong> from today.
+          </p>
           </label>
         </div>
 
+        {/* Question 2 */}
         <div>
           <p className={styles.question}>
             2. If you have passed any of the following qualifying National Interior Design Exams,
             please indicate so
-            <ul className="bullet-list">
-              <li>- ARE</li>
-              <li>- CASP</li>
-              <li>- LEED - AP (+)</li>
-              <li>- NCBDC</li>
-              <li>- NCIDQ</li>
-              <li>- NKBA - CKBD (+)</li>
+            <ul className={styles.ulprescreening}>
+              <li>ARE</li>
+              <li>CASP</li>
+              <li>LEED - AP (+)</li>
+              <li>NCBDC</li>
+              <li>NCIDQ</li>
+              <li>NKBA - CKBD (+)</li>
             </ul>
           </p>
 
           <br></br>
 
-          <input type="radio" id="op21" name="question2" />
+          <input type="radio" id="op21" name="question2" 
+          checked={question2Value === "op21"}
+          onChange={() => setQuestion2Value("op21")} />
+
           <label htmlFor="op21" className={styles.label}>Yes</label>
 
           
           <br></br>
 
-          <input type="radio" id="op22" name="question2" />
+          <input type="radio" id="op22" name="question2"
+          checked={question2Value === "op22"}
+          onChange={() => setQuestion2Value("op22")} />
+
           <label htmlFor="op22" className={styles.label}>No</label>
         </div>
 
+        {/* Question 3 */}
         <div>
           <p className={styles.question}>3. Do you mainly practice commercial design?</p>
 
           <br></br>
 
-          <input type="radio" id="op31" name="question3" />
+          <input type="radio" id="op31" name="question3"
+          checked={question3Value === "op31"}
+          onChange={() => setQuestion3Value("op31")} />
+
           <label htmlFor="op31" className={styles.label}>Yes</label>
 
          
           <br></br>
 
-          <input type="radio" id="op32" name="question3" />
+          <input type="radio" id="op32" name="question3"
+          checked={question3Value === "op32"} 
+          onChange={() => setQuestion3Value("op32")} />
           <label htmlFor="op32" className={styles.label}>No</label>
         </div>
 
-        <NavLink className={styles.centeredContainer} to={"/prescreening"}>
-          <Button onClick={null}>Continue</Button>
-        </NavLink>
+
+        <div className={styles.centeredContainer}>
+        <button className={buttonStyle.button} type="submit" form="form1" value="Submit">Submit</button>
+        </div>
+        
       </form>
     </Page>
   );
