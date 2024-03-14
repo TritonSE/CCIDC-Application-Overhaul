493abd9cc0c0e534107baf0b2b6f6faabe28ee26
Sidhant Rohatgi <rohatgisidhant@gmail.com>
2024-03-13 18:36:25 -0700

    Linting Fix post merge conflicts

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,8 +3,8 @@ import { Button } from "./Button.tsx";
 import { FAQs } from "./FAQs.tsx";
 import { NavBar } from "./NavBar.tsx";
 import { Page } from "./Page.tsx";
-import { PrescreeningForm } from "./PrescreeningForm.tsx";
 import { Pathway } from "./Pathway.tsx";
 import { PathwayTimeline } from "./PathwayTimeline.tsx";
+import { PrescreeningForm } from "./PrescreeningForm.tsx";
 
 export { Page, Button, NavBar, Accordion, Pathway, FAQs, PathwayTimeline, PrescreeningForm };
