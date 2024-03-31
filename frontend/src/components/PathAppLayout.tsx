1de9122c823e66c005bba91b09db810cfe48ad31
Jared Rosas <jaredsbt@gmail.com>
2024-03-31 16:04:27 -0700

    Fixed arrow sizing and color change on hover

--- frontend/src/components/PathAppLayout.tsx
+++ frontend/src/components/PathAppLayout.tsx
@@ -1,5 +1,5 @@
 import styles from "./PathAppLayout.module.css";
-import { Button, PathwayTimeline } from "./index.ts";
+import { Button, PathwayTimeline, Page } from "./index.ts";
 import arrow from "../assets/arrow.svg";
 import toggledArrow from "../assets/search.svg";
 import backArrow from "../assets/backArrow.svg";
@@ -8,11 +8,11 @@ import toggledBackArrow from "../assets/toggledBackArrow.svg";
 import { useState } from "react";
 
 export type PathAppLayoutProps = {
-  pathName: string;
+  path: 1 | 2 | 3 | 4;
   children: React.ReactNode;
 };
 
-export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ pathName, children }) => {
+export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path, children }) => {
   const [pageNum, setPageNum] = useState<0 | 1 | 2 | 3 | 4 | 5>(0);
 
   const next = () => {
@@ -28,39 +28,42 @@ export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ pathName, children
   };
 
   return (
-    <div className={styles.pathwayApplicationBase}>
-      <div className={styles.applicationContainer}>
-        <h1 className={styles.title}>{pathName}</h1>
+    <Page>
+      <div className={styles.pathwayApplicationBase}>
+        <div className={styles.applicationContainer}>
+          <h1 className={styles.title}>Path {path} Application</h1>
 
-        <div className={styles.pathContent}>{children}</div>
-        <div className={styles.centeredContainer}>
-          <Button onClick={null}>Retake Prescreening Questions</Button>
+          <div className={styles.pathContent}>{children}</div>
+          <div className={styles.centeredContainer}>
+            <Button onClick={null}>Retake Prescreening Questions</Button>
+          </div>
+
+          <div className={styles.note}>
+            <p className={styles.red}>
+              {" "}
+              If you would like to be sorted into a different pathway, please click above to retake
+              the
+              <br></br>
+              sorting questionnaire. For further inquiries, please reach out to ccidc@ccidc.org
+            </p>
+          </div>
         </div>
 
-        <div className={styles.note}>
-          <p className={styles.red}>
-            {" "}
-            If you would like to be sorted into a different pathway, please click above to retake
-            the
-            <br></br>
-            sorting questionnaire. For further inquiries, please reach out to ccidc@ccidc.org
-          </p>
+        <PathwayTimeline path={1} progress={pageNum}></PathwayTimeline>
+
+        <div className={styles.navigationContainer}>
+          <button onClick={back}>
+            <div className={styles.backArrow}>
+              <img src={backArrow} id={styles.backArrow}></img>
+            </div>
+          </button>
+          <button onClick={next}>
+            <div className={styles.arrow}>
+              <img src={arrow} id={styles.arrow}></img>
+            </div>
+          </button>
         </div>
       </div>
-
-      <PathwayTimeline path={1} progress={pageNum}></PathwayTimeline>
-      <div className={styles.pageNavigator}>
-        <button onClick={back}>
-          <div className={styles.backArrow}>
-            <img src={backArrow} id={styles.backArrow}></img>
-          </div>
-        </button>
-        <button onClick={next}>
-          <div className={styles.arrow}>
-            <img src={arrow} id={styles.arrow}></img>
-          </div>
-        </button>
-      </div>
-    </div>
+    </Page>
   );
 };
