46b7b47710252c9506f723f39d5cf6251bb253df
Jared Rosas <jaredsbt@gmail.com>
2024-03-31 17:48:29 -0700

    Path title and description updates given selected path

--- frontend/src/components/PathAppLayout.tsx
+++ frontend/src/components/PathAppLayout.tsx
@@ -7,10 +7,9 @@ import { useState } from "react";
 
 export type PathAppLayoutProps = {
   path: 1 | 2 | 3 | 4;
-  children: React.ReactNode;
 };
 
-export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path, children }) => {
+export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path }) => {
   const [pageNum, setPageNum] = useState<0 | 1 | 2 | 3 | 4 | 5>(0);
 
   const next = () => {
@@ -25,13 +24,59 @@ export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path, children }) 
     }
   };
 
+  let path_descriptions = {
+    1: (
+      <p>
+        Path 1 is for Applicants who meet minimum education or experience-only requirements but who
+        are still in the process of completing the necessary education/work experience, may take the
+        <span className={styles.red}>IDEX California® Examination</span>. After successfully
+        passing the <span className={styles.red}>IDEX California® Examination</span>, these
+        candidates must submit the required proof of education/work experience, as listed under the
+        Categories of Certification, in order to complete the certification process.
+      </p>
+    ),
+
+    2: (
+      <p>
+        Path 2 is for Applicants who meet minimum education and education/work experience, may take
+        the <span className={styles.red}>IDEX California® Examination</span>. After successfully
+        passing the <span className={styles.red}>IDEX California® Examination</span>, these
+        candidates must submit the required proof of education/work experience, as listed under the
+        Categories of Certification, in order to complete the certification process.
+      </p>
+    ),
+
+    3: (
+      <p>
+        Path 3 is for Applicants who meet minimum education and education/work experience, and has
+        passed one of the qualifying National Interior Designer Exams. Applicants are eligible and
+        must take the <span className={styles.red}>IDEX California® Examination</span>. After
+        successfully passing the <span className={styles.red}>IDEX California® Examination</span>,
+        these candidates must submit the required proof of education/work experience, as listed
+        under the Categories of Certification, in order to complete the certification process.
+      </p>
+    ),
+
+    4: (
+      <p>
+        Path 4 is for Applicants who meet minimum education and education/work experience, have
+        passed one of the qualifying National Interior Designer Exams, and passed required ICC
+        courses. Applicants are eligible and must take the{" "}
+        <span className={styles.red}>IDEX California® Examination</span>. After successfully
+        passing the <span className={styles.red}>IDEX California® Examination</span>, these
+        candidates must submit the required proof of education/work experience, as listed under the
+        Categories of Certification, in order to complete the certification process.
+      </p>
+    ),
+  };
+
   return (
     <Page>
       <div className={styles.pathwayApplicationBase}>
         <div className={styles.applicationContainer}>
           <h1 className={styles.title}>Path {path} Application</h1>
 
-          <div className={styles.pathContent}>{children}</div>
+          <div className={styles.pathContent}>{path_descriptions[path]}</div>
           <div className={styles.centeredContainer}>
             <Button onClick={null}>Retake Prescreening Questions</Button>
           </div>
@@ -47,7 +92,7 @@ export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path, children }) 
           </div>
         </div>
 
-        <PathwayTimeline path={1} progress={pageNum}></PathwayTimeline>
+        <PathwayTimeline path={path} progress={pageNum}></PathwayTimeline>
 
         <div className={styles.navigationContainer}>
           <button onClick={back}>
--- frontend/src/pages/ApplyPage.tsx
+++ frontend/src/pages/ApplyPage.tsx
@@ -1,13 +1,5 @@
 import { PathAppLayout } from "../components/index.ts";
 
 export function ApplyPage() {
-  return (
-    <PathAppLayout pathName="Path 1 Application">
-      Path 1 is for Applicants who meet minimum education or experience-only requirements but who
-      are still in the process of completing the necessary education/work experience, may take the
-      IDEX California® Examination. After successfully passing the IDEX California® Examination,
-      these candidates must submit the required proof of education/work experience, as listed under
-      the Categories of Certification, in order to complete the certification process.
-    </PathAppLayout>
-  );
+  return <PathAppLayout path={1}></PathAppLayout>;
 }
