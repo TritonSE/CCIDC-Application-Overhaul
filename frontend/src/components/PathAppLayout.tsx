f48bd5c3906920d69195c8a7afd78ae42a9ef002
Jared Rosas <jaredsbt@gmail.com>
2024-03-31 18:07:11 -0700

    Font and padding changes

--- frontend/src/components/PathAppLayout.tsx
+++ frontend/src/components/PathAppLayout.tsx
@@ -28,7 +28,7 @@ export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path }) => {
     1: (
       <p>
         Path 1 is for Applicants who meet minimum education or experience-only requirements but who
-        are still in the process of completing the necessary education/work experience, may take the
+        are still in the process of completing the necessary education/work experience, may take the{" "}
         <span className={styles.red}>IDEX California® Examination</span>. After successfully
         passing the <span className={styles.red}>IDEX California® Examination</span>, these
         candidates must submit the required proof of education/work experience, as listed under the
@@ -71,42 +71,43 @@ export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path }) => {
   };
 
   return (
-    <Page>
-      <div className={styles.pathwayApplicationBase}>
-        <div className={styles.applicationContainer}>
-          <h1 className={styles.title}>Path {path} Application</h1>
+    <div className={styles.pathwayApplicationBase}>
+      <div className={styles.applicationContainer}>
+        <h1 className={styles.title}>Path {path} Application</h1>
 
-          <div className={styles.pathContent}>{path_descriptions[path]}</div>
-          <div className={styles.centeredContainer}>
-            <Button onClick={null}>Retake Prescreening Questions</Button>
-          </div>
-
-          <div className={styles.note}>
-            <p className={styles.red}>
-              {" "}
-              If you would like to be sorted into a different pathway, please click above to retake
-              the
-              <br></br>
-              sorting questionnaire. For further inquiries, please reach out to ccidc@ccidc.org
-            </p>
-          </div>
+        <div className={styles.pathContent}>{path_descriptions[path]}</div>
+        <div className={styles.centeredContainer}>
+          <Button onClick={null}>Retake Prescreening Questions</Button>
         </div>
 
-        <PathwayTimeline path={path} progress={pageNum}></PathwayTimeline>
-
-        <div className={styles.navigationContainer}>
-          <button onClick={back}>
-            <div className={styles.backArrow}>
-              <img src={backArrow} id={styles.backArrow}></img>
-            </div>
-          </button>
-          <button onClick={next}>
-            <div className={styles.arrow}>
-              <img src={arrow} id={styles.arrow}></img>
-            </div>
-          </button>
+        <div className={styles.note}>
+          <p className={styles.red}>
+            {" "}
+            If you would like to be sorted into a different pathway, please click above to retake
+            the
+            <br></br>
+            sorting questionnaire. For further inquiries, please reach out to{" "}
+            <strong>ccidc@ccidc.org</strong>
+          </p>
         </div>
       </div>
-    </Page>
+
+      <PathwayTimeline path={path} progress={pageNum}></PathwayTimeline>
+
+      <div className={styles.navigationContainer}>
+        <button onClick={back}>
+          <div className={styles.backArrow}>
+            <img src={backArrow} id={styles.backArrow}></img>
+          </div>
+        </button>
+        <button onClick={next}>
+          <div className={styles.arrow}>
+            <img src={arrow} id={styles.arrow}></img>
+          </div>
+        </button>
+      </div>
+    </div>
+    // <Page>
+    // </Page>
   );
 };
