e2e6239304d7c8f72bdca23fe3b966cdc44dfe6d
Jared Rosas <jaredsbt@gmail.com>
2024-03-31 18:31:29 -0700

    Uses page component, removed padding

--- frontend/src/components/PathAppLayout.tsx
+++ frontend/src/components/PathAppLayout.tsx
@@ -71,43 +71,43 @@ export const PathAppLayout: React.FC<PathAppLayoutProps> = ({ path }) => {
   };
 
   return (
-    <div className={styles.pathwayApplicationBase}>
-      <div className={styles.applicationContainer}>
-        <h1 className={styles.title}>Path {path} Application</h1>
+    <Page>
+      <div className={styles.pathwayApplicationBase}>
+        <div className={styles.applicationContainer}>
+          <h1 className={styles.title}>Path {path} Application</h1>
 
-        <div className={styles.pathContent}>{path_descriptions[path]}</div>
-        <div className={styles.centeredContainer}>
-          <Button onClick={null}>Retake Prescreening Questions</Button>
+          <div className={styles.pathContent}>{path_descriptions[path]}</div>
+          <div className={styles.centeredContainer}>
+            <Button onClick={null}>Retake Prescreening Questions</Button>
+          </div>
+
+          <div className={styles.note}>
+            <p className={styles.red}>
+              {" "}
+              If you would like to be sorted into a different pathway, please click above to retake
+              the
+              <br></br>
+              sorting questionnaire. For further inquiries, please reach out to{" "}
+              <strong>ccidc@ccidc.org</strong>
+            </p>
+          </div>
         </div>
 
-        <div className={styles.note}>
-          <p className={styles.red}>
-            {" "}
-            If you would like to be sorted into a different pathway, please click above to retake
-            the
-            <br></br>
-            sorting questionnaire. For further inquiries, please reach out to{" "}
-            <strong>ccidc@ccidc.org</strong>
-          </p>
+        <PathwayTimeline path={path} progress={pageNum}></PathwayTimeline>
+
+        <div className={styles.navigationContainer}>
+          <button onClick={back}>
+            <div className={styles.backArrow}>
+              <img src={backArrow} id={styles.backArrow}></img>
+            </div>
+          </button>
+          <button onClick={next}>
+            <div className={styles.arrow}>
+              <img src={arrow} id={styles.arrow}></img>
+            </div>
+          </button>
         </div>
       </div>
-
-      <PathwayTimeline path={path} progress={pageNum}></PathwayTimeline>
-
-      <div className={styles.navigationContainer}>
-        <button onClick={back}>
-          <div className={styles.backArrow}>
-            <img src={backArrow} id={styles.backArrow}></img>
-          </div>
-        </button>
-        <button onClick={next}>
-          <div className={styles.arrow}>
-            <img src={arrow} id={styles.arrow}></img>
-          </div>
-        </button>
-      </div>
-    </div>
-    // <Page>
-    // </Page>
+    </Page>
   );
 };
