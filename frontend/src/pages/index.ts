83e9b1729e5329461827428da0445b0c96a43835
Jared Rosas <jaredsbt@gmail.com>
2024-04-08 12:15:15 -0700

    Merge complete

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -4,4 +4,4 @@ import { Apply } from "./Apply.tsx";
 import { Candidates } from "./Candidates.tsx";
 import { TestCongratulations } from "./TestCongratulations.tsx";
 
-export { Candidates, Apply, ThankyouForApplying };
+export { Candidates, Apply, TestCongratulations, ThankyouForApplying };
