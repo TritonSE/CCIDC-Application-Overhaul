df7fbdb9760da943d7c7195a6cb093578aec452b
Jared Rosas <jaredsbt@gmail.com>
2024-04-09 01:17:14 -0700

    remomved unnecessary div tag. Final pr changes

--- frontend/src/pages/Application.tsx
+++ frontend/src/pages/Application.tsx
@@ -82,18 +82,15 @@ export const Application: React.FC<ApplicationProps> = ({ path }: ApplicationPro
           </Button>
         </div>
 
-        <div className={styles.note}>
-          <p className={styles.red}>
-            {" "}
-            If you would like to be sorted into a different pathway, please click above to retake
-            the
-            <br></br>
-            sorting questionnaire. For further inquiries, please reach out to{" "}
-            <a href="mailto:ccidc@ccidc.org" className={styles.red}>
-              <strong>ccidc@ccidc.org</strong>
-            </a>
-          </p>
-        </div>
+        <p className={styles.note}>
+          {" "}
+          If you would like to be sorted into a different pathway, please click above to retake the
+          <br></br>
+          sorting questionnaire. For further inquiries, please reach out to{" "}
+          <a href="mailto:ccidc@ccidc.org" className={styles.red}>
+            <strong>ccidc@ccidc.org</strong>
+          </a>
+        </p>
       </div>
 
       <div className={styles.formContainer}>
