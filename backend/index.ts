447081969e450d29c7e1ac139d6c86fb3dd0baac
mohakvni <mohakvaswani@gmail.com>
2024-04-12 16:24:45 -0700

    fixed backend changes and added cors contrl middleware

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import fileRoutes from "./src/routes/fileRoutes";
-// const fileRoutes = require("./src/routes/fileRoutes.ts");
+import fileRoutes from "./src/routes/driveRoutes.ts";
+import cors from 'cors';
 
 const PORT: string | number = process.env.PORT ?? 3001;
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/file", fileRoutes);
