e25bbd4e4555ebb11c6771141ea6cc6e8d2160a6
mohakvni <mohakvaswani@gmail.com>
2024-04-12 16:24:45 -0700

    fixed backend changes and added cors contrl middleware

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -12,6 +13,7 @@ const PORT: string | number = process.env.PORT ?? 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
