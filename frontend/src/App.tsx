27440503960be748d2b17839a6c0f726a024aca4
Tikpai <hpai@ucsd.edu>
2024-04-15 13:54:35 -0700

    small changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet, Route, Routes } from "react-router-dom";
 
 import { NavBar } from "./components/index.ts";
-import { Apply, Candidates, TestCongratulations, FileTesting } from "./pages/index.ts";
+import { Apply, Candidates, FileTesting, TestCongratulations } from "./pages/index.ts";
 
 function Layout() {
   return (
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,6 @@
 import { Apply } from "./Apply.tsx";
 import { Candidates } from "./Candidates.tsx";
-import { TestCongratulations } from "./TestCongratulations.tsx";
 import { FileTesting } from "./FileTesting.tsx";
+import { TestCongratulations } from "./TestCongratulations.tsx";
 
 export { Candidates, Apply, TestCongratulations, FileTesting };
