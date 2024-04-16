2b949cee9ebabdeee33d632bad9a3d80db69dd61
mbansal03 <122562063+mbansal03@users.noreply.github.com>
2024-04-15 20:52:03 -0700

    Fixed an import that imported a non-existing file

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,4 @@
 import { Application } from "./Application.tsx";
-import { Apply } from "./Apply.tsx";
 import { Candidates } from "./Candidates.tsx";
 import { PrescreeningForm } from "./PrescreeningForm.tsx";
 import { TestCongratulations } from "./TestCongratulations.tsx";
