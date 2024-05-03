4a866387276276b71b3c0978fd6077c4ef593018
Sidhant Rohatgi <rohatgisidhant@gmail.com>
2024-05-02 18:53:39 -0700

    Added Step 3 to Paths

--- frontend/src/components/Step3.tsx
+++ frontend/src/components/Step3.tsx
@@ -3,183 +3,207 @@ import { WorkExperienceForm } from "./index.ts";
 import { Button } from "./Button.tsx";
 import { Link } from "react-router-dom";
 
+// Pass in Application's next function
+export type StepProps = {
+  next: () => void;
+};
 
-export function Step3() {
+export const Step3: React.FC<StepProps> = ({ next }: StepProps) => {
+
+  // Checks if all Required Inputs are filled before moving on to next step
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    if (!form.checkValidity()) {
+      return;
+    }
+    next();
+  }
+
   return (
-    <div className={styles.formContainer}>
+    <>
+      <form id="step3-form" onSubmit={onSubmit}>
 
-      <hr />
+        <div className={styles.formContainer}>
 
-      <div className={styles.formSection}>
-        <WorkExperienceForm
-          sectionName="Current Company Info"
-          formInputs={[
-            {
-              inputTitle: "Company Name",
-              defaultMessage: "Add Company Name",
-              inputType: "text",
-            },
-            {
-              inputTitle: "Website",
-              defaultMessage: "Add your Company's Website",
-              inputType: "text",
-            },
-            {
-              inputTitle: "Profession",
-              defaultMessage: "Add Profession Here",
-              inputType: "text",
-            },
-            {
-              inputTitle: "Specialization",
-              defaultMessage: "Add Specialization Here",
-              inputType: "text",
-            },
-          ]}
-        />
-      </div>
-      <hr />
-      <div className={styles.formSection}>
-        <WorkExperienceForm
-          sectionName="Work Experience"
-          formInputs={[
-            {
-              inputTitle: "Recent diversified design experience",
-              defaultMessage: "Design Experience",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "How many hours per week did you work (on average)?",
-              defaultMessage: "Number of Hours",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Name of Superviser to Contact",
-              defaultMessage: "Superviser Name",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Supervisor’s Phone Number",
-              defaultMessage: "Enter Phone Number",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Supervisor’s Email Address",
-              defaultMessage: "Enter Supervisor’s Email Address",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Company Name",
-              defaultMessage: "Add Company Name",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Address",
-              defaultMessage: "Enter Address",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "City",
-              defaultMessage: "Enter City",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "State",
-              defaultMessage: "Enter State",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Zip",
-              defaultMessage: "Enter Zip",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Country",
-              defaultMessage: "Enter Country",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Hire Date",
-              defaultMessage: "mm/dd/yyyy",
-              inputType: "text",
-              required: false,
-            },
-            {
-              inputTitle: "Last Date Worked",
-              defaultMessage: "mm/dd/yyyy",
-              inputType: "text",
-              required: false,
-            },
-          ]}
-          add={true}
-        />
-      </div>
-      <hr />
-      <div className={styles.formSection}>
-        <WorkExperienceForm
-          sectionName="Total Work Experience"
-          formInputs={[
-            {
-              inputTitle: "Total Hours Work Experience",
-              defaultMessage: "Number of Hours",
-              inputType: "text",
-              required: true,
-            },
-            {
-              inputTitle: "Total Years Work Experience",
-              defaultMessage: "Number of Years",
-              inputType: "text",
-              required: true,
-            },
+        <hr />
 
-          ]}
-        />
-      </div>
-      <hr />
-      <div className={styles.formSection}>
-        <WorkExperienceForm
-          sectionName="Upload Proof of Diversified Interior Design Experience (5+ years of experience)"
-          formInputs={[
-          ]}
+        <div className={styles.formSection}>
+          <WorkExperienceForm
+            sectionName="Current Company Info"
+            formInputs={[
+              {
+                inputTitle: "Company Name",
+                defaultMessage: "Add Company Name",
+                inputType: "text",
+              },
+              {
+                inputTitle: "Website",
+                defaultMessage: "Add your Company's Website",
+                inputType: "text",
+              },
+              {
+                inputTitle: "Profession",
+                defaultMessage: "Add Profession Here",
+                inputType: "text",
+              },
+              {
+                inputTitle: "Specialization",
+                defaultMessage: "Add Specialization Here",
+                inputType: "text",
+              },
+            ]}
+          />
+        </div>
+        <hr />
+        <div className={styles.formSection}>
+          <WorkExperienceForm
+            sectionName="Work Experience"
+            formInputs={[
+              {
+                inputTitle: "Recent diversified design experience",
+                defaultMessage: "Design Experience",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "How many hours per week did you work (on average)?",
+                defaultMessage: "Number of Hours",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Name of Superviser to Contact",
+                defaultMessage: "Superviser Name",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Supervisor’s Phone Number",
+                defaultMessage: "Enter Phone Number",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Supervisor’s Email Address",
+                defaultMessage: "Enter Supervisor’s Email Address",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Company Name",
+                defaultMessage: "Add Company Name",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Address",
+                defaultMessage: "Enter Address",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "City",
+                defaultMessage: "Enter City",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "State",
+                defaultMessage: "Enter State",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Zip",
+                defaultMessage: "Enter Zip",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Country",
+                defaultMessage: "Enter Country",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Hire Date",
+                defaultMessage: "mm/dd/yyyy",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Last Date Worked",
+                defaultMessage: "mm/dd/yyyy",
+                inputType: "text",
+                required: true,
+              },
+            ]}
+            add={true}
+          />
+        </div>
+        <hr />
+        <div className={styles.formSection}>
+          <WorkExperienceForm
+            sectionName="Total Work Experience"
+            formInputs={[
+              {
+                inputTitle: "Total Hours Work Experience",
+                defaultMessage: "Number of Hours",
+                inputType: "text",
+                required: true,
+              },
+              {
+                inputTitle: "Total Years Work Experience",
+                defaultMessage: "Number of Years",
+                inputType: "text",
+                required: true,
+              },
+
+            ]}
+          />
+        </div>
+        <hr />
+        <div className={styles.formSection}>
+          <WorkExperienceForm
+            sectionName="Upload Proof of Diversified Interior Design Experience (5+ years of experience)"
+            formInputs={[
+            ]}
+            
+          />
+
+          <p>
+          i. IF YOU ARE SELF-EMPLOYED – Complete the{" "} 
+
+          <Link to="https://ccidc.org/wp-content/uploads/2023/05/Work-Verification-Form.pdf" className={styles.blueLink}>
+            Employment Verification Form
+          </Link>{" "}
+
+          and include Tax Returns that claims yourself as an Interior Designer in the occupation section going back the amount of years you are claiming experience for or letter from a CPA or Attorney.
+          </p>
+
+          <p>
+          ii. IF YOU ARE NOT SELF-EMPLOYED – Fill out the{" "}
+
+          <Link to="https://ccidc.org/wp-content/uploads/2023/05/Work-Verification-Form.pdf" className={styles.blueLink}>
+            Employment Verification Form
+          </Link>{" "}
           
-        />
+          must be signed by Candidate and Employer.
+          </p>
 
-        <p>
-        i. IF YOU ARE SELF-EMPLOYED – Complete the{" "} 
+          <Button onClick={undefined} additionalStyle={styles.upload}>
+              Upload Employment Verification Form
+          </Button>
 
-        <Link to="https://ccidc.org/wp-content/uploads/2023/05/Work-Verification-Form.pdf" className={styles.blueLink}>
-          Employment Verification Form
-        </Link>{" "}
+        </div>
 
-        and include Tax Returns that claims yourself as an Interior Designer in the occupation section going back the amount of years you are claiming experience for or letter from a CPA or Attorney.
-        </p>
+        </div>
 
-        <p>
-        ii. IF YOU ARE NOT SELF-EMPLOYED – Fill out the{" "}
 
-        <Link to="https://ccidc.org/wp-content/uploads/2023/05/Work-Verification-Form.pdf" className={styles.blueLink}>
-          Employment Verification Form
-        </Link>{" "}
-        
-        must be signed by Candidate and Employer.
-        </p>
+      </form>
 
-        <Button onClick={undefined} additionalStyle={styles.upload}>
-            Upload Employment Verification Form
-        </Button>
-
-      </div>
-
-    </div>
+    </>
   );
 }
