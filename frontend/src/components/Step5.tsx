0a5fe6040056238c48441533518b2502658e8094
Sidhant Rohatgi <rohatgisidhant@gmail.com>
2024-05-09 18:02:57 -0700

    Fixed Unused Imports and CSS

--- frontend/src/components/Step5.tsx
+++ frontend/src/components/Step5.tsx
@@ -1,8 +1,5 @@
-import { Link } from "react-router-dom";
-
 import { Button } from "./Button.tsx";
 import styles from "./Step5.module.css";
-import { WorkExperienceForm } from "./index.ts";
 
 // Pass in Application's next function
 export type StepProps = {
@@ -24,43 +21,45 @@ export const Step5: React.FC<StepProps> = ({ next }: StepProps) => {
   return (
     <>
       <form id="step5-form" onSubmit={onSubmit}>
-      <div className={styles.formContainer}>
-        <hr />
-        <div className={styles.formSection}>
+        <div className={styles.formContainer}>
+          <hr />
+          <div className={styles.formSection}>
             <p>
-            To pay via  <strong className={styles.red}> credit card </strong>: Navigate to the Payment Portal using the button below <br />
-            Please Note: In the required “Invoice” field, please enter: IDEX – Candidates Name <br />
-            <strong className={styles.red}> Mail checks </strong> to: CCIDC, Inc. – 365 W. Second Ave, Suite 221, Escondido, CA 92025
+              To pay via <strong className={styles.red}> credit card </strong>: Navigate to the
+              Payment Portal using the button below <br />
+              Please Note: In the required “Invoice” field, please enter: IDEX – Candidates Name{" "}
+              <br />
+              <strong className={styles.red}> Mail checks </strong> to: CCIDC, Inc. – 365 W. Second
+              Ave, Suite 221, Escondido, CA 92025
             </p>
 
             <br />
 
             <p className={styles.red}>
-            Total Fees due with application (Includes Application Fee + IDEX Exam Fee + Testing Center Fee)**: <strong>$700 Total*</strong>
+              Total Fees due with application (Includes Application Fee + IDEX Exam Fee + Testing
+              Center Fee)**: <strong>$700 Total*</strong>
             </p>
 
             <br />
 
-            <p>     
-            <span className={styles.red}>
-            *
-            </span>
-            Fee is nonrefundable <br />
-            <span className={styles.red}>
-            **
-            </span>
-            Application fees must be submitted with application.
+            <p>
+              <span className={styles.red}>*</span>
+              Fee is nonrefundable <br />
+              <span className={styles.red}>**</span>
+              Application fees must be submitted with application.
             </p>
 
             <br />
 
-            <Button onClick={undefined} additionalStyle={styles.paymentportalbutton}>
-            Payment Portal
-            </Button>
+            <div className={styles.centerButton}>
+              <Button onClick={undefined} additionalStyle={styles.paymentportalbutton}>
+                Payment Portal
+              </Button>
+            </div>
+          </div>
 
+          <hr />
         </div>
-        <hr />
-      </div>
       </form>
     </>
   );
