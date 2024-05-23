e323870a0e21aaafdbeb6b08581749c1fbb96d75
Helen <59126064+Miyuki-L@users.noreply.github.com>
2024-05-22 22:00:11 -0700

    removed extra import

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,13 +2,11 @@ import { Application } from "./Application.tsx";
 import { Candidates } from "./Candidates.tsx";
 import { Login } from "./Login.tsx";
 import { PrescreeningForm } from "./PrescreeningForm.tsx";
-import { TestCongratulations } from "./TestCongratulations.tsx";
 import { TestModals } from "./TestModals.tsx";
 import { ThankyouForApplying } from "./ThankyouForApplying.tsx";
 export {
   Application,
   Candidates,
-  TestCongratulations,
   ThankyouForApplying,
   PrescreeningForm,
   Login,
