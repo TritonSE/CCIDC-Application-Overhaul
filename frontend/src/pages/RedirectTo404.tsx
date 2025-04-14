fe45e2d7c752306a8b77bd3c6350e352dda3aaba
benjaminjohnson2204 <ben332004@gmail.com>
2025-04-14 16:27:57 -0700

    Update wording on 404 page

--- frontend/src/pages/RedirectTo404.tsx
+++ frontend/src/pages/RedirectTo404.tsx
@@ -8,10 +8,11 @@ export function RedirectTo404() {
         <h1 className={styles.errorType}>404</h1>
         <h2 className={styles.errorReason}>Page Not Found</h2>
         <p className={styles.errorMessage}>
-          Sorry, we couldn&apos;t find the page you&apos;re looking for, click{" "}
+          Sorry, we couldn&apos;t find the page you&apos;re looking for. Click{" "}
           <a className={styles.lightBlue} href="https://ccidc.org/">
-            here.
-          </a>
+            here
+          </a>{" "}
+          to return to the home page.
           <br />
           Please ensure that the URL is correct and try again.
         </p>
